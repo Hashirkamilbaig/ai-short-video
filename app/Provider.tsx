502e1ef6d98a7f50e50ab1bf7104a502e17b69c3
hashirkamilbaig <hashirkamilbaig@gmail.com>
2024-12-17 00:55:45 -0800

    Implemented clerk, sign-in page customized and also made a provider file

---
+++ app/Provider.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react';
+
+interface ProviderProps {
+  children: ReactNode;
+}
+
+const Provider = ({ children }: ProviderProps) => {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+export default Provider;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello</h1>
       <Button variant="destructive">Button</Button>
+
+      <UserButton />
     </div>
   );
 }
